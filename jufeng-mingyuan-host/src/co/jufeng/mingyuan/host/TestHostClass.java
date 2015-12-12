95f46b5aceaa1739fa43ba326bb2c94c68c0a8d1
jufeng <jufeng@localhost.localdomain>
2015-12-13 01:09:26 +0800

    ff

--- jufeng-mingyuan-host/src/co/jufeng/mingyuan/host/TestHostClass.java
+++ jufeng-mingyuan-host/src/co/jufeng/mingyuan/host/TestHostClass.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class TestHostClass {
-    //kkkk
+    //kkkkuuu
     public void testMethod(Context context) {
         Toast.makeText(context, "Successed invoke host method", Toast.LENGTH_SHORT).show();
     }
